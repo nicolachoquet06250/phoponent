31e7631fc6b55ee02aa82816cd993d1a19d6e149
nicolas <nicolachoquet06250@gmail.com>
2018-07-10 21:45:39 +0200

    ajout du system mvc pour chaque composant

--- components/Clock/Clock.php
+++ components/Clock/Clock.php
@@ -2,7 +2,6 @@
 
 class Clock extends xphp_tag {
 	public function render(): string {
-	    $this->get_model('first_model')->echo_toto();
 	    $result = $this->get_view('horloge')->render();
 		return $result;
 	}
