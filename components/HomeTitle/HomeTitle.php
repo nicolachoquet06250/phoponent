0e18bfae0949cba9f5b772809b2c85bc290141c5
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-07-12 13:49:00 +0200

    amélioration du système estétiquement et dynamisation du composant tableau en fonction des collonnes du fichier json en parametre

--- components/HomeTitle/HomeTitle.php
+++ components/HomeTitle/HomeTitle.php
@@ -2,9 +2,11 @@
 
 class HomeTitle extends xphp_tag {
 	public function render(): string {
-        $title = $this->get_model('Title')->get_title($this->attribute('TitleView'));
-		return $this->get_view('HomeTitle')->set_vars([
-															   'TitleView' => $title,
-        ])->render();
+        $title = $this->get_model('Title')
+					  ->get_title($this->attribute('title'));
+		return $this->get_view('HomeTitle')
+					->set_vars([
+						'title' => $title,
+					])->render();
 	}
 }
